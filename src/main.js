8fba584d24f9bdd40dd73fbb7d86695e1893f6f6
Encep Suryana <encep.suryanajr@gmail.com>
2021-02-18 14:14:13 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,47 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+//import bootstrap css & js
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery/dist/jquery.min'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min'
+
+//import vue router
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+//import component
+import PostIndex from "./components/posts/Index";
+import PostCreate from "./components/posts/Create";
+import PostEdit from "./components/posts/Edit";
+
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: "/",
+      name: "posts",
+      component: PostIndex,
+    },
+    {
+      path: "/create",
+      name: "/create",
+      component: PostCreate,
+    },
+    {
+      path: "/edit/:id",
+      name: "edit",
+      component: PostEdit,
+    },
+  ],
+  mode: "history",
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
